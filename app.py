780f3060f97ce09bfb138086cde9179e2f889fe4
david-crushbank <61594716+david-crushbank@users.noreply.github.com>
2024-11-13 10:29:10 -0500

    fetch

--- app.py
+++ app.py
@@ -32,6 +32,44 @@ def require_apikey(f):
 
     return decorated_function
 
+def test_apikey(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get('x-api-key')
+        if api_key and api_key == VALID_API_KEY:
+            return f(*args, **kwargs)
+        else:
+            if api_key and api_key == VALID_API_KEY2:
+                return f(*args, **kwargs)
+            else:
+                return jsonify({"message": "Invalid or missing API key"}), 403
+
+    return decorated_function
+
+def fetch_secret():
+    # Connect to the database# Replace these with your database details
+    server = '38.66.76.155'  # e.g., 'yourserver.database.windows.net'
+    username = 'cb_digitalocean'
+    password = '@19digitaL*oceaN$83'
+    database = 'CrushBank'
+
+    conn = pymssql.connect(server=server, user=username, password=password, database=database)
+    print("Connection successful!")
+    cursor = conn.cursor(as_dict=True)  # Use `as_dict=True` to get results as dictionaries
+
+    # Execute a query
+    cursor.execute("select companyID from company where CompanyUuId = 'ce299ec9-0a9e-4fd9-bf80-00c542c36d8c'")
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+    print("Connection closed.")
+
+    return results
+
 def fetch_data():
     # Connect to the database# Replace these with your database details
     server = '38.66.76.155'  # e.g., 'yourserver.database.windows.net'
@@ -95,11 +133,11 @@ def process_data():
 @app.route('/test')
 def index():
     # Fetch data from the database
-    data = fetch_data()
+    data = fetch_secret()
 
     # Pass data to the HTML template
-    return ('this is a test')
-    return render_template('index.html', data=data)
+    return data
+    #return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
