3fd01a4bbe44932e448cba8bb0ae2e708eaa5066
david <david@crushbank.com>
2024-11-15 09:21:46 -0500

    add colon to substring command

--- app.py
+++ app.py
@@ -72,10 +72,10 @@ def require_apikey(f):
         clientid = json_data.get('clientid')
         plaintext = clientid + api_key
         encrypted_text = encrypt(plaintext)
-        print(clientid)
-        print(plaintext)
-        print(encrypted_text)
-        if api_key and encrypted_text[105] == fetch_secret(clientid)[105]:
+        #print(clientid)
+        #print(plaintext)
+        #print(encrypted_text)
+        if api_key and encrypted_text[:105] == fetch_secret(clientid)[:105]:
             return f(*args, **kwargs)
         else:
             return jsonify({"message": "Invalid or missing API key"}), 403
