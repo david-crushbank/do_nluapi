c486f365751f401f12ead62b0d24225138eb5499
david-crushbank <61594716+david-crushbank@users.noreply.github.com>
2024-12-17 02:48:58 +0000

    halo

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def log_request(company_id,ticket_id,halo_id):
     #print(sql, values)
 
     #cursor.execute(sql, values)
-    cursor.execute('NSERT INTO ClassificationLog (CompanyUuid, TicketNumber, HaloID) VALUES (%s, %s, %s)', (company_id,ticket_id,halo_id,))
+    cursor.execute('INSERT INTO ClassificationLog (CompanyUuid, TicketNumber, HaloID) VALUES (%s, %s, %s)', (company_id,ticket_id,halo_id,))
 
     # Close the cursor and connection
     cursor.close()
